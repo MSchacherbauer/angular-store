a916e9b04282605c4252c58a2147cff7f9d36e6e
Max Schacherbauer <maxi.schacherbauer@gmail.com>
2023-01-01 20:14:58 +0100

    Add cart view

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Product } from '../products';
+import { Product, products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -10,7 +11,17 @@ import { Product } from '../products';
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
   ngOnInit(): void {
-
+    const routeParas = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParas.get('productId'));
+    this.product = products.find(p => p.id === productIdFromRoute);
   }
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) { }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
 }
